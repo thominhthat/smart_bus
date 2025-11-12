583ffc862a649ede96dc556a2bc8e9b6929fc789
thominhthat <93632688+thominhthat@users.noreply.github.com>
2025-11-13 02:53:43 +0700

    Improve serial command processing for LED matrix
    Refactor command handling to use a buffer for incoming serial data.

--- NanoLEDmaxtrix.ino
+++ NanoLEDmaxtrix.ino
@@ -1,6 +1,14 @@
+/* 
+  Arduino Nano 2 - Nhận lệnh LED từ Mega
+  - MAX7219 LED matrix
+  - Nhận lệnh qua Serial từ Mega
+  - Lệnh "SCROLL:..." để cuộn chữ
+  - Lệnh "CLEAR" để xóa màn hình
+*/
+
+#include <SPI.h>
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
-#include <SPI.h>
 
 #define MAX_DEVICES 4
 #define MD_PIN_DATA 11
@@ -9,27 +17,39 @@
 
 MD_Parola matrix = MD_Parola(MD_MAX72XX::FC16_HW, MD_PIN_DATA, MD_PIN_CLK, MD_PIN_CS, MAX_DEVICES);
 
+String incoming = "";
+
 void setup() {
-  Serial.begin(115200);  // Nhận từ Mega
+  Serial.begin(115200); // Nhận dữ liệu từ Mega
   matrix.begin();
   matrix.setIntensity(4);
   matrix.displayClear();
 }
 
 void loop() {
-  if (Serial.available()) {
-    String cmd = Serial.readStringUntil('\n');
-    cmd.trim();
-
-    if (cmd == "CLEAR") {
-      matrix.displayClear();
-    } else if (cmd.startsWith("SCROLL:")) {
-      String msg = cmd.substring(7);
-      matrix.displayClear();
-      matrix.displayScroll(msg.c_str(), PA_CENTER, PA_SCROLL_LEFT, 50);
-      unsigned long start = millis();
-      while (millis() - start < 3000) matrix.displayAnimate();
-      matrix.displayClear();
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n' || c == '\r') {
+      processCommand(incoming);
+      incoming = "";
+    } else {
+      incoming += c;
     }
+  }
+}
+
+void processCommand(String cmd) {
+  cmd.trim();
+  if (cmd.length() == 0) return;
+
+  if (cmd.startsWith("SCROLL:")) {
+    String msg = cmd.substring(7);
+    matrix.displayClear();
+    matrix.displayScroll(msg.c_str(), PA_CENTER, PA_SCROLL_LEFT, 50);
+    unsigned long start = millis();
+    while (millis() - start < 3000) matrix.displayAnimate();
+    matrix.displayClear();
+  } else if (cmd == "CLEAR") {
+    matrix.displayClear();
   }
 }
